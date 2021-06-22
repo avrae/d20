e863aa099480a96b95c400fdd92c5c8af8fe0b15
Andrew Zhu <me@andrew-zhu.com>
2021-06-22 13:32:59 -0700

    ci: oops

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True, scope="function")
-async def global_fixture():
+def global_fixture():
     """Seed each individual test with the same seed, so that different runs of the same test are deterministic"""
     random.seed(42)
     yield
